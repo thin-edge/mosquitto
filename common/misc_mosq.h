621d74fd6a119f1c68b9c79aa6cdd7efbc9a0e6d
Norbert Heusser <norbert.heusser@cedalo.com>
2022-06-10 17:12:03 +0000

    Created new helper function mosquitto_write_file in common/misc_mosq.h to consolidate saving config files in failsafe manner
    Signed-off-by: Norbert Heusser <norbert.heusser@cedalo.com>

--- common/misc_mosq.h
+++ common/misc_mosq.h
@@ -25,4 +25,6 @@ FILE *mosquitto__fopen(const char *path, const char *mode, bool restrict_read);
 char *misc__trimblanks(char *str);
 char *fgets_extending(char **buf, int *buflen, FILE *stream);
 
+int mosquitto_write_file(const char* target_path, bool restrict_read, int (*write_fn)(FILE* fptr, void* user_data), void* user_data, void (*log_fn)(const char* msg));
+
 #endif
