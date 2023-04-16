3c6ad454895f7a07082d44c1241ed3c89c8eaf08
Roger A. Light <roger@atchoo.org>
2023-04-16 19:35:02 +0100

    Fix leak on error.
    Closes oss-fuzz #57948.

--- src/handle_connect.c
+++ src/handle_connect.c
@@ -1049,6 +1049,7 @@ int handle__connect(struct mosquitto *context)
 
 
 handle_connect_error:
+	mosquitto_property_free_all(&properties);
 	mosquitto__FREE(auth_data);
 	mosquitto__FREE(clientid);
 	mosquitto__FREE(username);
