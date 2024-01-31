69ee7da10600bd6ae0c4da0a26c1f7703dc0c72f
Roger A. Light <roger@atchoo.org>
2024-01-31 16:58:55 +0000

    Simplify base64_mosq.c

--- common/base64_mosq.c
+++ common/base64_mosq.c
@@ -39,33 +39,30 @@ int base64__encode(const unsigned char *in, size_t in_len, char **encoded)
 {
 	BIO *bmem, *b64;
 	BUF_MEM *bptr = NULL;
+	int rc = 1;
 
 	b64 = BIO_new(BIO_f_base64());
 	if(b64 == NULL) return 1;
 
 	BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
 	bmem = BIO_new(BIO_s_mem());
-	if(bmem == NULL){
-		BIO_free_all(b64);
-		return 1;
+	if(bmem){
+		b64 = BIO_push(b64, bmem);
+		BIO_write(b64, in, (int)in_len);
+
+		if(BIO_flush(b64) == 1){
+			BIO_get_mem_ptr(b64, &bptr);
+			*encoded = malloc(bptr->length+1);
+			if(*encoded){
+				memcpy(*encoded, bptr->data, bptr->length);
+				(*encoded)[bptr->length] = '\0';
+				rc = 0;
+			}
+		}
 	}
-	b64 = BIO_push(b64, bmem);
-	BIO_write(b64, in, (int)in_len);
-	if(BIO_flush(b64) != 1){
-		BIO_free_all(b64);
-		return 1;
-	}
-	BIO_get_mem_ptr(b64, &bptr);
-	*encoded = malloc(bptr->length+1);
-	if(!(*encoded)){
-		BIO_free_all(b64);
-		return 1;
-	}
-	memcpy(*encoded, bptr->data, bptr->length);
-	(*encoded)[bptr->length] = '\0';
 	BIO_free_all(b64);
 
-	return 0;
+	return rc;
 }
 
 
@@ -74,42 +71,34 @@ int base64__decode(const char *in, unsigned char **decoded, unsigned int *decode
 	BIO *bmem, *b64;
 	size_t slen;
 	int len;
+	int rc = 1;
 
 	slen = strlen(in);
 
 	b64 = BIO_new(BIO_f_base64());
-	if(!b64){
-		return 1;
-	}
+	if(!b64) return 1;
+
 	BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-
 	bmem = BIO_new(BIO_s_mem());
-	if(!bmem){
-		BIO_free_all(b64);
-		return 1;
-	}
-	b64 = BIO_push(b64, bmem);
-	BIO_write(bmem, in, (int)slen);
+	if(bmem){
+		b64 = BIO_push(b64, bmem);
+		BIO_write(bmem, in, (int)slen);
 
-	if(BIO_flush(bmem) != 1){
-		BIO_free_all(b64);
-		return 1;
-	}
-	*decoded = calloc(slen, 1);
-	if(!(*decoded)){
-		BIO_free_all(b64);
-		return 1;
-	}
-	len = BIO_read(b64, *decoded, (int)slen);
-	BIO_free_all(b64);
+		if(BIO_flush(bmem) == 1){
+			*decoded = calloc(slen, 1);
 
-	if(len <= 0){
-		SAFE_FREE(*decoded);
-		*decoded_len = 0;
-		return 1;
+			if(*decoded){
+				len = BIO_read(b64, *decoded, (int)slen);
+				if(len > 0){
+					*decoded_len = (unsigned int)len;
+					rc = 0;
+				}else{
+					SAFE_FREE(*decoded);
+				}
+			}
+		}
 	}
-	*decoded_len = (unsigned int)len;
 
-	return 0;
+	return rc;
 }
 #endif
