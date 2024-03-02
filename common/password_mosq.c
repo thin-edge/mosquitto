34e07184e008981d0cd7afa847a4b628f3d55131
Roger A. Light <roger@atchoo.org>
2024-03-02 19:40:45 +0000

    Fix oss-fuzz 67167.
    Leak on startup and reload only, assuming a corrupt password file
    provided by the admin. Minimal impact.

--- common/password_mosq.c
+++ common/password_mosq.c
@@ -436,6 +436,7 @@ static int pw__decode_sha512(struct mosquitto_pw *pw, const char *salt_password)
 	rc = base64__decode(salt_b64, &salt, &salt_len);
 	if(rc != MOSQ_ERR_SUCCESS || (salt_len != 12 && salt_len != HASH_LEN)){
 		free(sp_heap);
+		free(salt);
 		return MOSQ_ERR_INVAL;
 	}
 	memcpy(pw->params.sha512.salt, salt, salt_len);
